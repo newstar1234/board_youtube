1c6234ebf8d4183322739bbfc9c9af625bf27511
newstar <maori36@naver.com>
2023-11-29 14:34:04 +0900

    23.11.29

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,12 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import latestBoardListMock from 'mocks/latest-board-list.mock';
-import Top3Item from 'components/Top3Item';
-import { commentListMock, favoriteListMock, top3BoardListMock } from 'mocks';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import Footer from 'layouts/Footer';
 
 function App() {
 
@@ -14,7 +8,7 @@ function App() {
 
   return (
     <>
-      <InputBox label='email' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error ={true} setValue={setValue} message='다시 확인해주세요.'/>
+      <Footer />    
     </>
   );
 }
