dbbd50e79fd6f92fc845751507fe36c8c148c846
newstar <maori36@naver.com>
2023-12-15 20:42:14 +0900

    23.12.15

--- board-front/src/components/Pagination/index.tsx
+++ board-front/src/components/Pagination/index.tsx
@@ -7,6 +7,25 @@ export default function Pagination() {
 
   // render //
   return (
-    <div></div>
+    <div id='pagination-wrapper'>
+      <div className='pagination-change-link-box'>
+        <div className='icon-box-small'>
+          <div className='icon expand-left-light-icon'></div>
+        </div>
+        <div className='pagination-change-link-text'>{'이전'}</div>
+      </div>
+      <div className='pagination-divider'>{'\|'}</div>
+
+      <div className='pagination-text-active'>{1}</div>
+      <div className='pagination-text'>{2}</div>
+
+      <div className='pagination-divider'>{'\|'}</div>
+      <div className='pagination-change-link-box'>
+        <div className='pagination-change-link-text'>{'다음'}</div>
+        <div className='icon-box-small'>
+          <div className='icon expand-right-light-icon'></div>
+        </div>
+      </div>
+    </div>
   )
 }
