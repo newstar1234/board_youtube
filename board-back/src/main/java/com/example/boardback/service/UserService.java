8d4e8ec37dec7d2b07228fe3655e31d5cf25e546
newstar <maori36@naver.com>
2023-12-22 12:13:42 +0900

    23.12.22

--- board-back/src/main/java/com/example/boardback/service/UserService.java
+++ board-back/src/main/java/com/example/boardback/service/UserService.java
@@ -3,9 +3,11 @@ package com.example.boardback.service;
 import org.springframework.http.ResponseEntity;
 
 import com.example.boardback.dto.response.user.GetSignInUserResponseDto;
+import com.example.boardback.dto.response.user.GetUserResponseDto;
 
 public interface UserService {
-  
+
+  ResponseEntity<? super GetUserResponseDto> getUser(String email);
   ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
 
 }
--- board-back/src/main/java/com/example/boardback/service/implement/UserServiceImplement.java
+++ board-back/src/main/java/com/example/boardback/service/implement/UserServiceImplement.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.boardback.dto.response.ResponseDto;
 import com.example.boardback.dto.response.user.GetSignInUserResponseDto;
+import com.example.boardback.dto.response.user.GetUserResponseDto;
 import com.example.boardback.entity.UserEntity;
 import com.example.boardback.repository.UserRepository;
 import com.example.boardback.service.UserService;
@@ -16,6 +17,25 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImplement implements UserService {
 
   private final UserRepository userRepository;
+  
+
+  @Override
+  public ResponseEntity<? super GetUserResponseDto> getUser(String email) {
+
+    UserEntity userEntity = null;
+
+    try {
+
+      userEntity = userRepository.findByEmail(email);
+      if(userEntity == null) return GetUserResponseDto.noExistUser();
+
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return GetUserResponseDto.success(userEntity);
+  }
 
   @Override
   public ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email) {
