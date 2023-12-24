cea31f51e5792109d6099fa29080f9324b29d6e7
newstar <maori36@naver.com>
2023-12-24 17:27:45 +0900

    23.12.24

--- board-front/src/views/User/index.tsx
+++ board-front/src/views/User/index.tsx
@@ -1,20 +1,30 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './style.css';
 import DefaultImage from 'assets/image/urshifu.jpg';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BoardListItem } from 'types/interface';
+import { latestBoardListMock } from 'mocks';
+import BoardItem from 'components/BoardItem';
+import { BOARD_PATH, BOARD_WRITE_PATH, MAIN_PATH, USER_PATH } from 'constant';
+import { useLoginUserStore } from 'stores';
 
 export default function User() {
 
-  // state : 마이페이지 여부 상태 //
+  // state : user email path variable 상태 //
   const { userEmail} = useParams();
+  // state : 로그인 유저 상태 //
+  const { loginUser } = useLoginUserStore();
+  // state : 마이페이지 여부 상태 //
+  const [isMyPage, setMyPage] = useState<boolean>(true);
+
+  // function : 네비게이터 함수 //
+  const navigator = useNavigate();
   
   // component : 유저 화면 상단 //
   const UserTop = () => {
 
     // state : 이미지 파일 인풋 참조 상태 //
     const imageInputRef = useRef<HTMLInputElement | null>(null);
-    // state : 마이페이지 여부 상태 //
-    const [isMyPage, setMyPage] = useState<boolean>(true);
     // state : 닉네임 변경 여부 상태 //
     const [isNicknameChange, setNicknameChange] = useState<boolean>(false);
     // state : 닉네임 상태 //
@@ -96,9 +106,59 @@ export default function User() {
 
   // component : 유저 화면 하단 //
   const UserBottom = () => {
+
+    // state : 게시물 갯수 상태 //
+    const [count, setCount] = useState<number>(5);
+    // state : 게시물 리스트 상태 //
+    const [userBoardList, setUserBoardList] = useState<BoardListItem[]>([]);
+
+    // event handler : 사이드 카드 클릭 이벤트 처리 //
+    const onSideCardClickHandler = () => {
+      if(isMyPage) navigator(BOARD_PATH() + '/' + BOARD_WRITE_PATH());
+      else if(loginUser) navigator(USER_PATH(loginUser.email));
+    }
+
+    // effect : userEmail path variable이 바뀔때마다 실행될 함수 //
+    useEffect(() => {
+      setUserBoardList(latestBoardListMock);
+    }, [userEmail]);
+
     // render //
     return (
-      <div></div>
+      <div id='user-bottom-wrapper'>
+        <div className='user-bottom-container'>
+          <div className='user-bottom-title'>{isMyPage ? '내 게시물 ' : '게시물 '}<span className='emphasis'>{count}</span></div>
+          <div className='user-bottom-contents-box'>
+            {count === 0 ? 
+            <div className='user-bottom-contents-nothing'>{'게시물이 없습니다.'}</div> : 
+            <div className='user-bottom-contents'>
+              {userBoardList.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}
+            </div>
+            }
+            <div className='user-bottom-side-box'>
+              <div className='user-bottom-side-card' onClick={onSideCardClickHandler} >
+                <div className='user-bottom-side-container'>
+                  {isMyPage ? 
+                    <>
+                    <div className='icon-box'>
+                      <div className='icon edit-icon'></div>
+                    </div>
+                    <div className='user-bottom-side-text'>{'글쓰기'}</div>
+                    </> :
+                    <>
+                    <div className='user-bottom-side-text'>{'내 게시물로 가기'}</div>
+                    <div className='icon-box'>
+                      <div className='icon arrow-right-icon'></div>
+                    </div>
+                    </>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='user-bottom-pagination-box'></div>
+        </div>
+      </div>
     )
   };
 
