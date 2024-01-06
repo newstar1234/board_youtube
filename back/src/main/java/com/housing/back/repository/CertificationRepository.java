222054cab85f68bbc5bd781ef8770fdc86033d5b
newstar <maori36@naver.com>
2024-01-06 16:44:00 +0900

    24.01.06.oauth

---
+++ back/src/main/java/com/housing/back/repository/CertificationRepository.java
@@ -0,0 +1,9 @@
+package com.housing.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.housing.back.entity.CertificationEntity;
+
+public interface CertificationRepository extends JpaRepository<CertificationEntity, String> {
+  
+}
