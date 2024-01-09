35d47fb9d019a17d4ce837071159df3ccd62d4fd
newstar <maori36@naver.com>
2024-01-09 15:46:06 +0900

    24.01.09.oauth

--- back/src/main/java/com/housing/back/filter/JwtAuthenticationFilter.java
+++ back/src/main/java/com/housing/back/filter/JwtAuthenticationFilter.java
@@ -39,48 +39,49 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         try {
 
           String token = parseBearerToken(request);
-          if(token == null) { // token이 null이면 넘어가지마라 //
+          if(token == null) { // token이 null이면 밑에 작업 진행하지말고 doFilter로 다음 필터로 넘기고 return //
             filterChain.doFilter(request, response);
             return;
           }
 
-          String userId = jwtProvider.validate(token);
-          if(userId == null) { // userId 가 null이면 넘어가지마라 // 검증완료
+          String userId = jwtProvider.validate(token); // token을 jwtProvider로 validate로 검증 -> 검증 통해서 userId 꺼내옴 
+          if(userId == null) { // userId 가 null이면 doFilter로 검증 
             filterChain.doFilter(request, response);
             return;
           }
 
-          UserEntity userEntity = userRepository.findByUserId(userId);
+          UserEntity userEntity = userRepository.findByUserId(userId); // user 정보 꺼내오기 
           String role = userEntity.getRole(); // role : ROLE_USER, ROLE_ADMIN // 권한 지정
 
-          // ROLE_DEVELOPER, ROLE_BOSS ... etc 배열형태
-          List<GrantedAuthority> authorities = new ArrayList<>();
+          // ROLE_DEVELOPER, ROLE_BOSS ... etc 권한의 배열형태
+          List<GrantedAuthority> authorities = new ArrayList<>(); // 권한 리스트
           authorities.add(new SimpleGrantedAuthority(role));
           
-          SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-          AbstractAuthenticationToken authenticationToken = 
-            new UsernamePasswordAuthenticationToken(userId, null, authorities);
-          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContext securityContext = SecurityContextHolder.createEmptyContext(); // 빈 context 만들어주기
+          AbstractAuthenticationToken authenticationToken =  // context 안에 담을 token 만들기
+            new UsernamePasswordAuthenticationToken(userId, null, authorities); // object를 받을수있음 // 유저정보, 비밀번호, 권한 리스트
+          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // request detail에 넣어줌
 
-          securityContext.setAuthentication(authenticationToken);
-          SecurityContextHolder.setContext(securityContext);
+          securityContext.setAuthentication(authenticationToken); // context에 token 넣어줌
+          SecurityContextHolder.setContext(securityContext); // context를 등록시켜줌
 
         } catch (Exception exception) {
           exception.printStackTrace();
         }
-
+        // 다음 filter로 넘어가도록 해줌
         filterChain.doFilter(request, response);
 
   }
 
+  // request 객체로부터 token 꺼내오는 작업 //
   private String parseBearerToken(HttpServletRequest request) {
 
-    String authorization = request.getHeader("Authorization");
+    String authorization = request.getHeader("Authorization"); // request의 header로 부터 Authorization 가져옴//
 
     boolean hasAuthorization = StringUtils.hasText(authorization);
     if(!hasAuthorization) return null;
 
-    boolean isBearer = authorization.startsWith("Bearer ");
+    boolean isBearer = authorization.startsWith("Bearer "); // 'Bearer '로 시작하는지 확인
     if(!isBearer) return null;
 
     String token = authorization.substring(7);
